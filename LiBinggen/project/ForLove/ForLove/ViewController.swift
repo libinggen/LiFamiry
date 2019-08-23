153a6d71dd8646ee7220e8ea31c51a26960b31f2
ZhangXingqing <zhangli_xingqing@163.com>
2019-08-24 06:34:18 +0900

    ForLove init

---
+++ LiBinggen/project/ForLove/ForLove/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ForLove
+//
+//  Created by LiBinggen on 2019/8/24.
+//  Copyright © 2019 qingqing. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ LiBinggen/project/ForLove/ForLoveTests/ForLoveTests.swift
@@ -0,0 +1,34 @@
+//
+//  ForLoveTests.swift
+//  ForLoveTests
+//
+//  Created by LiBinggen on 2019/8/24.
+//  Copyright © 2019 qingqing. All rights reserved.
+//
+
+import XCTest
+@testable import ForLove
+
+class ForLoveTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
